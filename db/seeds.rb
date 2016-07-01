386bca2a0b4d2e7ba0493377e42e1223e8b616ab
GettyGertie <gettyorawo@gmail.com>
2016-07-01 16:32:39 +0300

    adding sample users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create!(username: "stela gertie",
+             email: "getty@gmail.com",
+             password: "foobar",
+password_confirmation: "foobar")
+
+14.times do |n|
+  username = Faker::Name.name
+  email = "getty-#{n+1}@gmail.com"
+  password = "youuu"
+  User.create!(username: username,
+               email: email,
+               password: password,
+               password_confirmation: password)
+end
